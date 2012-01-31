338603ac8205e2bbc6de51f3dac52e938c5e79c0
Maxime Lévesque <maxime.levesque@gmail.com>
2012-01-31 11:08:52 -0500

    ...

--- project/project/Build.scala
+++ project/project/Build.scala
@@ -8,7 +8,11 @@ object PluginDef extends Build {
 	
 	lazy val root = Project("plugins", file(".")) dependsOn( pegdownPlugin )
 	
-	lazy val pegdownPlugin = file("C:/dev/sl-0.3/git-projects/pegdown-sbt")
+	// This dependency gives a null pointer at SBT startup time
 	
-	//lazy val pegdownPlugin = uri("git://github.com/max-l/pegdown-sbt.git")  
+	lazy val pegdownPlugin = uri("git://github.com/max-l/pegdown-sbt.git")  
+	
+	// If I clone the project, and depend via file, then it works :
+	
+	//lazy val pegdownPlugin = file("C:/dev/sl-0.3/git-projects/pegdown-sbt")
 }
